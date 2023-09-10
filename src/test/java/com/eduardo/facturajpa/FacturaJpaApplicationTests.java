eb61257ca4a06d69de3d9c791b228b6548ecc292
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-10 08:34:44 -0600

    Initial commit

---
+++ src/test/java/com/eduardo/facturajpa/FacturaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.facturajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
